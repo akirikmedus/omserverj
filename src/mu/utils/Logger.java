654767b45533aae57148963c8369cb5c915f3950
Andrew Kirik <andykirik@users.noreply.github.com>
2018-01-03 23:34:59 -0500

    logger

--- src/mu/utils/Logger.java
+++ src/mu/utils/Logger.java
@@ -3,11 +3,11 @@ package mu.utils;
 public class Logger {
     public void error(String str)
     {
-
+        System.out.println("ERROR: "+str);
     }
 
     public void info(String str)
     {
-
+        System.out.println("INFO: "+str);
     }
 }
